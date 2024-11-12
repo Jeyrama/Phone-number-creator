7195314ea060407998c86ce74b531f0c6d31257b
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-11 20:36:02 -0800

    Update Phone-number-creator.js

--- Phone-number-creator.js
+++ Phone-number-creator.js
@@ -11,3 +11,11 @@ Don't forget the space after the closing parentheses!
 
 
 // Solution
+
+function createPhoneNumber(numbers){
+  numbers = numbers.join('');
+  return '(' + numbers.substring(0, 3) + ') ' 
+      + numbers.substring(3, 6) 
+      + '-' 
+      + numbers.substring(6);
+}
