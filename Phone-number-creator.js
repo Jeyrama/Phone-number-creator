9eaac426c983d0ce01013eac80ca9a40809ed956
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-11 20:36:41 -0800

    Update Phone-number-creator.js

--- Phone-number-creator.js
+++ Phone-number-creator.js
@@ -21,3 +21,14 @@ function createPhoneNumber(numbers){
 }
 
 // or
+
+function createPhoneNumber(numbers){
+   let format = "(xxx) xxx-xxxx";
+  
+  for(let i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
